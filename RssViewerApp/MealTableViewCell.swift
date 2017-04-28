887e750632488c469aabe3576a004a7b26c97256
Ivan Yurchenko <ivan_yurchenko@icloud.com>
2017-04-28 19:43:09 +0300

    Added table view for displaying items

---
+++ RssViewerApp/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  RssViewerApp
+//
+//  Created by Ivan Yurchenko on 4/28/17.
+//  Copyright © 2017 Ivan Yurchenko. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
